febd29ca7ac6d826702966a17d151f95d6e256d3
dl-li <lidl2001.cn@gmail.com>
2026-08-01 17:37:04 +0800

    feat: set minimum window size to 640x360

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1535,7 +1535,7 @@ async fn open_in_new_window(
     )
     .title("LibreGene - Plasmid Editor")
     .inner_size(1400.0, 900.0)
-    .min_inner_size(480.0, 270.0)
+    .min_inner_size(640.0, 360.0)
     .decorations(true)
     .visible(false);
 
